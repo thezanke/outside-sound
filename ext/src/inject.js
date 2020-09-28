19f40a0a3623f60e31718f4d5d4bce57a85c6827
thezanke <thezanke@gmail.com>
2020-09-28 09:31:21 -0400

    clean up

--- ext/src/inject.js
+++ ext/src/inject.js
@@ -1,27 +1,25 @@
 const audioCtx = new AudioContext();
 
-// const feedForward = [0.00020298, 0.0004059599, 0.00020298];
-// const feedBack = [1.0126964558, -1.9991880801, 0.9873035442];
-// const filter = audioCtx.createIIRFilter(feedForward, feedBack);
-
 var filter = audioCtx.createBiquadFilter();
-filter.type = "lowpass"; // Low-pass filter. See BiquadFilterNode docs
-filter.frequency.value = 200; // Set cutoff to 440 HZ
+filter.type = "lowpass";
+filter.frequency.value = 200;
 
 let connected = false;
 let srcNode = null;
 
-const toggleActionHandler = (_request, _sender, sendResponse) => {
+const actionHandler = (_request, _sender, sendResponse) => {
   if (!connected) {
-    if (srcNode) {
-      srcNode.disconnect(audioCtx.destination);
-    } else {
+    if (!srcNode) {
       const myAudio = document.querySelector("audio,video");
-      srcNode = audioCtx.createMediaElementSource(myAudio);
+      if (myAudio) srcNode = audioCtx.createMediaElementSource(myAudio);
+    } else {
+      srcNode.disconnect(audioCtx.destination);
     }
 
-    srcNode.connect(filter).connect(audioCtx.destination);
-    connected = true;
+    if (srcNode) {
+      srcNode.connect(filter).connect(audioCtx.destination);
+      connected = true;
+    }
   } else {
     srcNode.disconnect(filter);
     srcNode.connect(audioCtx.destination);
@@ -31,11 +29,4 @@ const toggleActionHandler = (_request, _sender, sendResponse) => {
   sendResponse({ connected });
 };
 
-chrome.extension.sendMessage({}, function () {
-  const readyStateCheckInterval = setInterval(() => {
-    if (document.readyState === "complete") {
-      clearInterval(readyStateCheckInterval);
-      chrome.runtime.onMessage.addListener(toggleActionHandler);
-    }
-  }, 10);
-});
+chrome.runtime.onMessage.addListener(actionHandler);
